c96ba8eab7bcd66a8733fb51847a2b4baa74a102
alexSmkh <alexshikari9333@gmail.com>
2020-10-14 12:46:08 +0300

    Added testimonials section

--- js/main.js
+++ js/main.js
@@ -4,4 +4,15 @@ $(function() {
     dots: true,
     infinite: true,
   })
+
+  $('.works__inner').slick({
+    //infinite: true,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-left.svg" alt=""></button>',
+    nextArrow: '  <button class="slick-arrow slick-next"><img src="images/slider/slide-right.svg" alt=""></button>',
+    //centerMode: true,
+  })
 })
